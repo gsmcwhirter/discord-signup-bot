986d76f79e54064905ebc330f27436354ee24eaf
Gregory McWhirter <greg@ideafreemonoid.org>
2019-03-19 20:25:33 -0700

    Add database maintenance cmd

---
+++ cmd/trials-cleanup/app.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gsmcwhirter/discord-bot-lib/snowflake"
+	"github.com/gsmcwhirter/go-util/deferutil"
+	"github.com/pkg/errors"
+)
+
+type config struct {
+	Database string `mapstructure:"database"`
+	// User      string `mapstructure:"user"`
+	Guild string `mapstructure:"guild"`
+	// Channel   string `mapstructure:"channel"`
+	AllGuilds bool `mapstructure:"all_guilds"`
+}
+
+func start(c config) error {
+	fmt.Printf("%+v\n", c)
+
+	deps, err := createDependencies(c)
+	if err != nil {
+		return err
+	}
+	defer deps.Close()
+
+	gid, err := snowflake.FromString(c.Guild)
+	if err != nil {
+		return errors.Wrap(err, "could not parse guild id")
+	}
+
+	if err = cleanupGuildTrials(deps, gid, 200); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func cleanupGuildTrials(deps *dependencies, gid snowflake.Snowflake, maxlen int) error {
+	tx, err := deps.TrialAPI().NewTransaction(gid.ToString(), true)
+	if err != nil {
+		return errors.Wrap(err, "could not get trials transaction")
+	}
+	defer deferutil.CheckDefer(tx.Rollback)
+
+	ct := 0
+	trials := tx.GetTrials()
+	for _, t := range trials {
+		if len(t.GetName()) > maxlen {
+			fmt.Printf("Deleting `%s`\n", t.GetName())
+			if err := tx.DeleteTrial(t.GetName()); err != nil {
+				return errors.Wrap(err, "could not delete trial")
+			}
+			ct++
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return errors.Wrap(err, "could not commit deletes")
+	}
+
+	fmt.Printf("Deleted %d trials\n", ct)
+
+	return nil
+}
---
+++ cmd/trials-cleanup/dependencies.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	bolt "github.com/coreos/bbolt"
+	"github.com/go-kit/kit/log"
+
+	"github.com/gsmcwhirter/discord-signup-bot/pkg/storage"
+)
+
+type dependencies struct {
+	logger   log.Logger
+	db       *bolt.DB
+	trialAPI storage.TrialAPI
+	guildAPI storage.GuildAPI
+	// botSession *etfapi.Session
+}
+
+func createDependencies(conf config) (d *dependencies, err error) {
+	d = &dependencies{}
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "timestamp", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+	d.logger = logger
+
+	d.db, err = bolt.Open(conf.Database, 0660, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return
+	}
+
+	d.trialAPI, err = storage.NewBoltTrialAPI(d.db)
+	if err != nil {
+		return
+	}
+
+	d.guildAPI, err = storage.NewBoltGuildAPI(d.db)
+	if err != nil {
+		return
+	}
+
+	// d.botSession = etfapi.NewSession()
+
+	return
+}
+
+func (d *dependencies) Close() {
+	if d.db != nil {
+		d.db.Close() // nolint: errcheck
+	}
+}
+
+func (d *dependencies) Logger() log.Logger {
+	return d.logger
+}
+
+func (d *dependencies) TrialAPI() storage.TrialAPI {
+	return d.trialAPI
+}
+
+func (d *dependencies) GuildAPI() storage.GuildAPI {
+	return d.guildAPI
+}
+
+// func (d *dependencies) BotSession() *etfapi.Session {
+// 	return d.botSession
+// }
